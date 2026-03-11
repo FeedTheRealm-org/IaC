119be75eb2e44d0d141ef703fa1a976b5bc62563
maxogod <maxi18wars@gmail.com>
2026-03-11 20:36:25 -0300

    feat: add tag versions in github role

--- modules/identity/iam_github_actions_role/main.tf
+++ modules/identity/iam_github_actions_role/main.tf
@@ -12,7 +12,10 @@ resource "aws_iam_role" "this" {
       Condition = {
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}",
+            "repo:${var.github_org}/${var.github_repo}:ref:refs/tags/v*"
+          ]
         }
       }
     }]
